dc15b45b92083a25e1f0385c437300a1d338dc91
404404 <c547091843@hotmail.com>
2026-09-24 11:02:19 +0800

    fix: make SSH fixture host key startup noninteractive

--- test/fixtures/sshd/entrypoint.sh
+++ test/fixtures/sshd/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -eu
 mkdir -p /run/sshd
-ssh-keygen -q -t ed25519 -N '' -f /etc/ssh/ssh_host_ed25519_key
+if [ ! -s /etc/ssh/ssh_host_ed25519_key ]; then
+  ssh-keygen -q -t ed25519 -N '' -f /etc/ssh/ssh_host_ed25519_key
+fi
 exec /usr/sbin/sshd -D -e
